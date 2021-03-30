6849074044ec783953aed6822e282d102a20d315
Rohan Erasala <rohan@rohans-mbp-2.lan>
2021-03-29 21:36:44 -0400

    done

--- diss_9.py
+++ diss_9.py
@@ -6,36 +6,46 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    
+    a_tags = soup.find_all('a')
+
+    for el in a_tags:
+
+        names = str(el.get('href', None))
+
+        if 'charmander' in names:
+            return ('https://pokemondb.net' + names)
+
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
-    url = 'https://pokemondb.net/pokedex/'+pokemon
-    #add code here
+
+    link = 'https://pokemondb.net/pokedex/'+ pokemon
+    request = requests.get(link)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    tags = soup.find_all('a', class_='ent-name')
+
+    egg_moves = []
+    for el in tags:
+        egg_moves.append(el.text)
+
+    return (egg_moves)
 
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
     
+    times = []
 
-    # define the regular expression
+    for sentence in sentences:
+        time = re.findall('(?:@([0-9]{1,2}[ ]{0,1}[ap]m))', sentence)
+        if len(time) > 0:
+            times.append(time[0])
+
+    return (times)
     
-
-    # loop through each sentence or phrase in sentences
-    
-
-    # find all the words that match the regular expression in each sentence
-       
-
-    # loop through the found words and add the words to your empty list
-
-
-    #return the list of the last letter of all words that begin or end with a capital letter
-
-
-
 def main():
     url = 'https://pokemondb.net/pokedex/national'
     r = requests.get(url)
@@ -58,5 +68,6 @@ class TestAllMethods(unittest.TestCase):
         self.assertEqual(findLetters(['There is show @12pm if you want to join','I will be there @ 2pm', 'come at @3 pm will be better']), ['12pm', '3 pm'])
 
 if __name__ == "__main__":
+    findLetters(['Come eat lunch at 12','there"s a party @2pm', 'practice @7am','nothing'])
     main()
     unittest.main(verbosity = 2)
